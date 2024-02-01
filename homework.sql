e814c1ccdc3bee9945b38e6181d97839f063f67b
Michael  McEwing <107093761+Duke650@users.noreply.github.com>
2024-01-31 22:05:54 -0800

    finish

---
+++ homework.sql
@@ -0,0 +1,65 @@
+-- 1. List all customers who live in Texas (use JOINs)  
+
+SELECT first_name, last_name, city FROM customer
+JOIN address ON address.address_id = customer.address_id
+JOIN city ON address.city_id = city.city_id
+WHERE city.city = 'Texas'
+
+SELECT * FROM city
+-- A) There are no customers who live in Texas..
+
+-- 2. Get all payments above $6.99 with the Customer's FullName
+
+SELECT first_name, last_name, amount FROM customer
+JOIN payment ON customer.customer_id = payment.customer_id
+WHERE amount > 6.99
+
+-- 3. Show all customers names who have made payments over $175(use subqueries)
+
+SELECT first_name, last_name FROM customer
+WHERE customer_id IN (
+    SELECT customer_id FROM payment
+    GROUP BY first_name, last_name, customer_id
+    HAVING SUM(amount) > 175
+)
+
+-- 4. List all customers that live in Nepal (use the city table)
+
+SELECT first_name, last_name FROM customer
+JOIN address ON customer.address_id = address.address_id
+JOIN city ON address.city_id = city.city_id
+WHERE city.city = 'Nepal'
+
+-- A) NONE
+
+-- 5. Which staff member had the most transactions?
+
+SELECT first_name, last_name, COUNT(*) FROM staff
+JOIN payment ON staff.staff_id = payment.staff_id
+GROUP BY first_name, last_name
+ORDER BY count DESC
+
+-- A) Jon had the most transactions
+
+-- 6. How many movies of each rating are there?
+
+SELECT rating, COUNT(*) from film
+GROUP BY rating
+ORDER BY count
+
+-- 7.Show all customers who have made a single payment above $6.99 (Use Subqueries)
+
+SELECT first_name FROM customer
+WHERE customer_id IN (
+    SELECT customer_id FROM payment
+    WHERE amount > 6.99
+    GROUP BY first_name, customer_id
+    HAVING COUNT(*) = 1
+)
+
+-- 8. How many free rentals did our stores give away?
+
+SELECT COUNT(*) FROM payment
+WHERE amount = 0
+
+ -- A) 24 free rentals
